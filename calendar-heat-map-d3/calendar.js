e15c8a56baf9a2779efcd011dd841f80ad0fed1a
George Tang <gtang.gt@gmail.com>
2019-02-15 14:26:08 -0800

    Month Label and Interpolation Function

--- calendar-heat-map-d3/calendar.js
+++ calendar-heat-map-d3/calendar.js
@@ -2,6 +2,8 @@ const position = { x: 100, y: 100 }
 const square_side_length = 20;
 const square_spacing = 2;
 const days_in_week = 7
+const month = 'Feb';
+const interpolate = d3.scaleSequential((t) => { return d3.hsl(100, 1, t) } );
 
 const dataArray = [];
 
@@ -21,5 +23,14 @@ svg.selectAll("rect")
                 .append("rect")
                 .attr("height", square_side_length) 
                 .attr("width", square_side_length)
+                .attr('fill', (d, i) => { return interpolate(i / 42 + 0.01); })
                 .attr("x", (d,i) => { return (square_side_length + square_spacing) * Math.floor(i / days_in_week) + position.x; })
                 .attr("y", (d,i) => { return (square_side_length + square_spacing) * (i % days_in_week) + position.y });
+
+svg.selectAll('text')
+   .data([month])
+   .enter()
+   .append('text')
+   .attr('x', position.x)
+   .attr('y', position.y - 10)
+   .text((d) => {return d;});
