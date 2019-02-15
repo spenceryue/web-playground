5dcf36857fcf1d119831d385ee4cbee04b3170e0
George Tang <gtang.gt@gmail.com>
2019-02-15 15:08:10 -0800

    Days of the Week

--- calendar-heat-map-d3/calendar.js
+++ calendar-heat-map-d3/calendar.js
@@ -1,8 +1,10 @@
 const position = { x: 100, y: 100 }
-const square_side_length = 20;
-const square_spacing = 2;
+const square_side_length = 20
+const square_spacing = 2
 const days_in_week = 7
-const month = 'Feb';
+const month = 'Feb'
+const days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
 const interpolate = d3.scaleSequential((t) => { return d3.hsl(100, 1, t) } );
 
 const dataArray = [];
@@ -23,14 +25,25 @@ svg.selectAll("rect")
                 .append("rect")
                 .attr("height", square_side_length) 
                 .attr("width", square_side_length)
-                .attr('fill', (d, i) => { return interpolate(i / 42 + 0.01); })
+                .attr('fill', (d, i) => { return interpolate(((i % 2) + 0.5) / 2); })
                 .attr("x", (d,i) => { return (square_side_length + square_spacing) * Math.floor(i / days_in_week) + position.x; })
                 .attr("y", (d,i) => { return (square_side_length + square_spacing) * (i % days_in_week) + position.y });
 
-svg.selectAll('text')
+svg.selectAll('text.month')
    .data([month])
    .enter()
    .append('text')
+   .attr('class', 'month')
    .attr('x', position.x)
    .attr('y', position.y - 10)
+   .text((d) => {return d;});
+
+svg.selectAll('text.day')
+   .data(days_of_week)
+   .enter()
+   .append('text')
+   .attr('class', 'day')
+   .attr('font-size', '10px')
+   .attr('x', position.x - 40)
+   .attr('y', (d, i) => { return position.y + 12 + 22 * i; })
    .text((d) => {return d;});
