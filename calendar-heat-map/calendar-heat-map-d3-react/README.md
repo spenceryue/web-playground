c80abe900811dd373ae453a581425881c8e47228
George Tang <gtang.gt@gmail.com>
2019-02-15 21:48:51 -0800

    this is a playground
    interesting use of purpose to narrow down the ways to use a hammer

---
+++ calendar-heat-map/calendar-heat-map-d3-react/README.md
@@ -0,0 +1,14 @@
+# Same as D3 project
+
+create-react-app has some nice features namely:
+ -error detection
+ -no configure webserver
+ -other stuff that i am unaware of
+
+But this comes at the cost of the setup, it's a nuisance, I can't use my work computer because it's behind a proxy and it breaks the create-react-app process. When I use my personal computer, it just takes a long time to get it set up. There are a number of files/dependencies that take time to download, if I'm relying on a CSN, this code will never be production ready. This is a playground though.
+
+F12 error detection is fine anyways
+
+python -m SimpleHTTPServer is a good enough http server.
+
+
---
+++ calendar-heat-map/calendar-heat-map-d3-react/calendar.js
@@ -0,0 +1,70 @@
+const position = { x: 100, y: 100 }
+const square_side_length = 20
+const square_spacing = 2
+const days_in_week = 7
+const date = new Date();
+date.setDate(1);
+date.setMonth(0);
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const month = months[date.getMonth()];
+const days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+const interpolate = d3.scaleSequential((t) => { return d3.hsl(100, 1, t) } );
+
+const dataArray = [];
+
+console.log(date);
+
+for (let i = 0; i < 7 * 6; i++)
+{
+  if (months[date.getMonth()] !== month)
+  {
+    dataArray.push(1);
+    continue;
+  }
+
+  if (i % 7 == date.getDay())
+  {
+    dataArray.push(0);
+    date.setDate(date.getDate() + 1);
+  }
+  else
+  {
+    dataArray.push(1);
+  }
+}
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("height","100%")
+            .attr("width","100%");
+
+svg.selectAll("rect")
+    .data(dataArray)
+    .enter()
+    .append("rect")
+    .attr("height", square_side_length) 
+    .attr("width", square_side_length)
+    .attr('fill', (d, i) => { return interpolate(d + 0.9); })
+    .attr("x", (d,i) => { return (square_side_length + square_spacing) * Math.floor(i / days_in_week) + position.x; })
+    .attr("y", (d,i) => { return (square_side_length + square_spacing) * (i % days_in_week) + position.y });
+
+svg.selectAll('text.month')
+   .data([month])
+   .enter()
+   .append('text')
+   .attr('class', 'month')
+   .attr('x', position.x)
+   .attr('y', position.y - 10)
+   .text((d) => { return d; });
+
+svg.selectAll('text.day')
+   .data(days_of_week)
+   .enter()
+   .append('text')
+   .attr('class', 'day')
+   .attr('font-size', '10px')
+   .attr('x', position.x - 30)
+   .attr('y', (d, i) => { return position.y + 13 + 22 * i; })
+   .text((d) => { return d; });
