16f5178852873517412afb8d461867eb3217da8d
George Tang <gtang.gt@gmail.com>
2019-02-15 22:27:15 -0800

    weird
    The return statement for the react component needs to be on one line?
    Investigate tomorrow.
    I'm fried.

--- calendar-heat-map/calendar-heat-map-d3-react/calendar.js
+++ calendar-heat-map/calendar-heat-map-d3-react/calendar.js
@@ -1,3 +1,25 @@
+//testing to see that react works
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    //For some reason this line needs to be one line?
+    return <button onClick={()=>this.setState({liked:true })}>Like</button>;
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
 const position = { x: 100, y: 100 }
 const square_side_length = 20
 const square_spacing = 2
