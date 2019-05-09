42a011c4b839b4f0e2659a8cbe59fec760c4e6ec
George Tang <gtang.gt@gmail.com>
2019-05-09 00:45:31 -0700

    home page authorization

--- react-firebase-authentication/src/components/Home/index.js
+++ react-firebase-authentication/src/components/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { withAuthorization } from '../Session';
+
 const HomePage = () =>
   (
     <div>
-      Home Page
+      <h1>Home Page</h1>
+      <p>The Home Page is accessible by every signed in user.</p>
     </div>
   );
 
-export default HomePage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
