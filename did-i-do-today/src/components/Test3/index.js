6eacb43c3a1add5f0cd1d754e89a6bb219bb92c4
George Tang <gtang.gt@gmail.com>
2019-06-08 00:49:31 -0700

    caching current date

--- did-i-do-today/src/components/Test3/index.js
+++ did-i-do-today/src/components/Test3/index.js
@@ -9,8 +9,50 @@ class Test3Page extends Component {
     super(props);
     const date = new Date();
 
-    console.log(localStorage.setItem('myData', Math.random()));
-    console.log(localStorage.getItem('myData'));
+    this.state = {
+      data: JSON.parse(localStorage.getItem('myData'))
+    };
+  }
+
+  randomTrue() {
+    let num = Math.random();
+    if (num > 0.5)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  renderSubmit() {
+    let today = new Date();
+    let lastDate = new Date(JSON.parse(localStorage.getItem('lastDateSubmitted')));
+
+    if (today.getDate() === lastDate.getDate() ||
+        today.getMonth() === lastDate.getMonth() ||
+        today.getYear() === lastDate.getYear())
+    {
+    }
+    else
+    {
+      return (
+        <button onClick={() => {
+          localStorage.setItem('myData', JSON.stringify([
+            this.randomTrue(),
+            this.randomTrue(),
+            this.randomTrue(),
+            this.randomTrue(),
+            this.randomTrue(),
+            this.randomTrue(),
+            this.randomTrue()
+          ]));
+
+          localStorage.setItem('lastDateSubmitted', JSON.stringify(new Date()));
+
+        }}>
+          hello
+        </button>
+      );
+    }
   }
 
   render() {
@@ -19,8 +61,11 @@ class Test3Page extends Component {
         <div>
           Did I Make Progress On Did I Do Today?
         </div>
+        {this.renderSubmit()}
         <p/>
-        <Week dateIsStart={false} />
+
+        <Week dateIsStart={false} data={this.state.data} />
+
         <Quarter dateIsStart={false} />
         <Year dateIsStart={false} />
       </div>
