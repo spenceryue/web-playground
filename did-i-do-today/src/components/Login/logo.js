ff62b178537b684507133b828874266503644e95
George Tang <gtang.gt@gmail.com>
2019-06-09 14:01:40 -0700

    logo static

--- did-i-do-today/src/components/Login/logo.js
+++ did-i-do-today/src/components/Login/logo.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './logo.css';
 import * as TAGS from './tags';
 
+const num = Math.floor(Math.random() * TAGS.TAGS.length);
+
 const Logo = () => (
   <div>
   <p><b>D</b>id <b>I d</b>o.<b>t</b>oday</p>
-  <p className='tag'>{TAGS.TAGS[Math.floor(Math.random() * TAGS.TAGS.length)]}</p>
+  <p className='tag'>{TAGS.TAGS[num]}</p>
   </div>
 )
 
