b587e8dc90daf9db73cb81ce1f73bfda1fee32dd
George Tang <gtang.gt@gmail.com>
2019-06-29 13:41:55 -0700

    a ring is a commitment

--- rings/src/components/Ring/index.js
+++ rings/src/components/Ring/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './ring.css';
 
 const Ring = () => 
 (
   <svg
     height='100'
     width='100'
+    class='canvas'
   >
-    <path
+    <path class='ring'
       d='M50 34.0845
          a 15.9155 15.9155 0 0 1 0 31.831
          a 15.9155 15.9155 0 0 1 0 -31.831'
       fill='none'
       stroke='#444'
       stroke-width='1'
+      stroke-dasharray='75, 100'
     />
   </svg>
 )
