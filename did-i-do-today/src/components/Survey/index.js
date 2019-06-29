8f76fcfadf696ec548a7d003f31ab1597e2244a4
George Tang <gtang.gt@gmail.com>
2019-06-29 12:22:16 -0700

    field arrays and lunch

--- did-i-do-today/src/components/Survey/index.js
+++ did-i-do-today/src/components/Survey/index.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 
 import Debug from '../Debug';
 
-import { Formik, Form, Field, ErrorMessage }  from 'formik';
+import { Formik, Form, Field, FieldArray, ErrorMessage }  from 'formik';
 
 import * as yup from 'yup';
 
 const Survey = () => (
   <div>
     <h1>Survey</h1>
-    <Formik>
-      {({ values }) => 
+    <Formik
+      initialValues={{ questions: ['asdf', 'asdf', 'asdf' ]}}
+    >
+      {( formikProps ) => 
           (
-            <Form>
-            </Form>
+            <FieldArray 
+              name="questions"
+              component={SurveyForm}
+            />
           )
       }
     </Formik>
   </div>
 )
+
+const SurveyForm = ({ move, swap, push, insert, unshift, pop, form }) =>
+{
+  let array = [];
+  form.values.questions.forEach((question) => { array.push((<div>{question}</div>))});
+
+  return (
+    <Form>
+      {
+        array
+      }
+    </Form>
+  )
+}
 
 export default Survey;
