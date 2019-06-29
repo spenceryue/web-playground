d662cfa464ff11f58e5370383c72230a81f81804
George Tang <gtang.gt@gmail.com>
2019-06-29 13:30:24 -0700

    ring visualization
    a ring is a commitment, full-circle

---
+++ rings/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ring from './components/Ring'
+
+function App() {
+  return (
+    <div className="App">
+      <Ring/>
+    </div>
+  );
+}
+
+export default App;
---
+++ rings/src/components/Ring/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Ring = () => 
+(
+  <svg
+    height='100'
+    width='100'
+  >
+    <path
+      d='M50 34.0845
+         a 15.9155 15.9155 0 0 1 0 31.831
+         a 15.9155 15.9155 0 0 1 0 -31.831'
+      fill='none'
+      stroke='#444'
+      stroke-width='1'
+    />
+  </svg>
+)
+
+export default Ring;
